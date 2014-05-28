bd70ad0a8d6bcc1d1f10bc970a4f77f10bd1d856
hatemalimam <me@hatemalimam.com>
2014-05-28 17:28:06 +0200

    Changed groupId

---
+++ src/main/java/org/animatejsf/behavior/AnimateBehavior.java
@@ -0,0 +1,37 @@
+package org.animatejsf.behavior;
+
+import javax.faces.FacesException;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+import javax.faces.component.behavior.ClientBehaviorBase;
+import javax.faces.component.behavior.ClientBehaviorContext;
+import javax.faces.component.behavior.FacesBehavior;
+
+@FacesBehavior("org.animatejsf.behavior.animate")
+@ResourceDependencies({
+		@ResourceDependency(library = "animatejsf", name = "animate.css"),
+		@ResourceDependency(library = "animatejsf", name = "animatejsf.js") })
+public class AnimateBehavior extends ClientBehaviorBase {
+	protected String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String getScript(ClientBehaviorContext behaviorContext) {
+		//FacesContext ctx = behaviorContext.getFacesContext();
+		UIComponent parent = behaviorContext.getComponent();
+		String targetId = parent.getClientId();
+		if (type != null) {
+			return "animatejsf('" + targetId + "','"+ type +"')";
+		} else
+			throw new FacesException("Cannot find target component");
+		
+	}
+}
